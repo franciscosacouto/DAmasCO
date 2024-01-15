0a51cf9c93b3d2c21e05d3edadcedd5689ff110b
Simao Francisco <up201907198@up.pt>
2024-01-15 22:57:32 +0000

    dt and mlr

--- DT.py
+++ DT.py
@@ -5,34 +5,53 @@ from sklearn.tree import DecisionTreeRegressor  # Change to DecisionTreeRegresso
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
-from sklearn import tree
 import matplotlib.pyplot as plt
 import features
+from sklearn.preprocessing import MinMaxScaler
 
+
+def examine(y_pred, y_test):
+    c = pd.DataFrame({
+        'y' : y_test,
+        'p' : y_pred
+     })
+    c = c.sort_index()
+    plt.figure(figsize=[15,4])
+    plt.plot(c.y, color='green')
+    plt.plot(c.p, color='red')
+    plt.show()
+
+#Import of data
 sj=features.sj
 iq=features.iq
+scaler = MinMaxScaler()
 
-# Specify the columns you want to include in the new DataFrame
-selected_columns = [
-    'station_max_temp_c',  
-    'reanalysis_dew_point_temp_k',
-    'reanalysis_relative_humidity_percent',
-    'reanalysis_specific_humidity_g_per_kg',
-    'weekofyear',
-    'total_cases'
-]
+#Attributes used
+attributes = ['reanalysis_dew_point_temp_k', 'reanalysis_specific_humidity_g_per_kg', 'station_avg_temp_c',  'station_min_temp_c', 'weekofyear', 'total_cases']
 
 # Create a new DataFrame with the selected columns
-df = sj[selected_columns]
+df = sj[attributes]
 
-X_train, X_test, y_train, y_test = train_test_split(df.drop('total_cases', axis=1), df['total_cases'], random_state=0)
+#Division of training and test data
+X_train, X_test, y_train, y_test = train_test_split(df.drop('total_cases', axis=1), df['total_cases'], test_size = 0.2, random_state=0)
 
-clf = DecisionTreeRegressor(random_state=0)
+#Normalization of the data
+normalized_data = scaler.fit_transform(X_train)
+X_train = pd.DataFrame(normalized_data, columns=X_train.columns)
+normalized_data = scaler.fit_transform(X_test)
+X_test = pd.DataFrame(normalized_data, columns=X_test.columns)
+
+#Fitting the Decision Tree Model
+clf = DecisionTreeRegressor(min_samples_split=5, max_depth=50,random_state=0)
 clf.fit(X_train, y_train)
 
+#Prediction of validation set
 y_pred = clf.predict(X_test)
 
-# Use mean_absolute_error for regression
+#Graphs
+examine(y_pred,y_test)
+
+#Evaluate the model
 print("Mean Absolute Error:", mean_absolute_error(y_test, y_pred))
 mse = mean_squared_error(y_test, y_pred)
 print("Mean Squared Error:", mse)
@@ -41,9 +60,66 @@ print("Root Mean Squared Error:", rmse)
 r2 = r2_score(y_test, y_pred)
 print("R-squared:", r2)
 
-# Plot the decision tree
-fig = plt.figure(figsize=(15, 10))
-_ = tree.plot_tree(clf, 
-                   feature_names=list(df.columns[:-1]),  # Exclude the target variable
-                   filled=True)
-plt.show()
+#prediction model
+attributes2 = ['reanalysis_dew_point_temp_k', 'reanalysis_specific_humidity_g_per_kg', 'station_avg_temp_c',  'station_min_temp_c', 'weekofyear' ]
+rawfeats = pd.read_csv('data/dengue_features_test.csv')
+sj_validation = rawfeats[rawfeats.city=='sj'].copy()
+sj_validation = sj_validation[attributes2]
+sj_validation = sj_validation.interpolate(method='linear', limit_direction='forward')
+sj_norm = scaler.fit_transform(sj_validation)
+sj_validation = pd.DataFrame(sj_norm, columns=sj_validation.columns)
+y_validation_dt_sj= clf.predict(sj_validation)
+y_validation_dt_sj = y_validation_dt_sj.astype(int)
+
+
+
+#---iq--
+#Attributes used
+attributes = ['reanalysis_dew_point_temp_k', 'reanalysis_specific_humidity_g_per_kg', 'station_avg_temp_c',  'station_min_temp_c', 'weekofyear',  'total_cases']
+
+# Create a new DataFrame with the selected columns
+df = iq[attributes]
+
+#Division of training and test data
+X_train, X_test, y_train, y_test = train_test_split(df.drop('total_cases', axis=1), df['total_cases'], test_size = 0.2, random_state=0)
+
+#Normalization of the data
+normalized_data = scaler.fit_transform(X_train)
+X_train = pd.DataFrame(normalized_data, columns=X_train.columns)
+normalized_data = scaler.fit_transform(X_test)
+X_test = pd.DataFrame(normalized_data, columns=X_test.columns)
+
+#Fitting the Decision Tree Model
+clf = DecisionTreeRegressor(min_samples_split=5, max_depth=50,random_state=0)
+clf.fit(X_train, y_train)
+
+#Prediction of validation set
+y_pred = clf.predict(X_test)
+
+#Graphs
+examine(y_pred,y_test)
+
+# Evaluate the model
+print("Mean Absolute Error:", mean_absolute_error(y_test, y_pred))
+mse = mean_squared_error(y_test, y_pred)
+print("Mean Squared Error:", mse)
+rmse = np.sqrt(mse)
+print("Root Mean Squared Error:", rmse)
+r2 = r2_score(y_test, y_pred)
+print("R-squared:", r2)
+
+#prediction model
+attributes2 = ['reanalysis_dew_point_temp_k', 'reanalysis_specific_humidity_g_per_kg', 'station_avg_temp_c',  'station_min_temp_c', 'weekofyear']
+rawfeats = pd.read_csv('data/dengue_features_test.csv')
+iq_validation = rawfeats[rawfeats.city=='iq'].copy()
+iq_validation = iq_validation[attributes2]
+iq_validation = iq_validation.interpolate(method='linear', limit_direction='forward')
+iq_norm = scaler.fit_transform(iq_validation)
+iq_validation = pd.DataFrame(iq_norm, columns=iq_validation.columns)
+y_validation_dt_iq= clf.predict(iq_validation)
+y_validation_dt_iq = y_validation_dt_iq.astype(int)
+
+#submission
+submission = pd.read_csv('data/submission_format.csv',index_col=[0, 1, 2])
+submission.total_cases = np.concatenate([y_validation_dt_sj, y_validation_dt_iq])
+submission.to_csv("submissions/dt.csv")
--- MLR.py
+++ MLR.py
@@ -6,15 +6,76 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 import features
 
+def examine(y_pred, y_test):
+    c = pd.DataFrame({
+        'y' : y_test,
+        'p' : y_pred
+     })
+    c = c.sort_index()
+    plt.figure(figsize=[15,4])
+    plt.plot(c.y, color='green')
+    plt.plot(c.p, color='red')
+    plt.show()
+
+#Import of data
 sj = features.sj
 iq= features.iq
 
 #---sj---
-#print(sj.head())
 
 #Setting the value for X and Y
-x = sj[['reanalysis_dew_point_temp_k', 'reanalysis_specific_humidity_g_per_kg', 'station_avg_temp_c',  'station_min_temp_c']]
+attributes = ['reanalysis_dew_point_temp_k', 'reanalysis_specific_humidity_g_per_kg', 'station_avg_temp_c',  'station_min_temp_c']
+x = sj[attributes]
 y = sj['total_cases']
+
+#Splitting the dataset
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 100)
+
+#Fitting the Multiple Linear Regression model
+mlr = LinearRegression()  
+mlr.fit(x_train, y_train)
+
+#Intercept and Coefficient
+print("Intercept: ", mlr.intercept_)
+print("Coefficients:")
+print(list(zip(x, mlr.coef_)))
+
+#Prediction of test set
+y_pred_mlr= mlr.predict(x_test)
+
+#Graphs
+examine(y_pred_mlr,y_test)
+
+#Actual value and the predicted value
+mlr_diff = pd.DataFrame({'Actual value': y_test, 'Predicted value': y_pred_mlr})
+print(mlr_diff.head())
+
+#Model Evaluation
+from sklearn import metrics
+meanAbErr = metrics.mean_absolute_error(y_test, y_pred_mlr)
+meanSqErr = metrics.mean_squared_error(y_test, y_pred_mlr)
+rootMeanSqErr = np.sqrt(metrics.mean_squared_error(y_test, y_pred_mlr))
+print('R squared: {:.2f}'.format(mlr.score(x,y)*100))
+print('Mean Absolute Error:', meanAbErr)
+print('Mean Square Error:', meanSqErr)
+print('Root Mean Square Error:', rootMeanSqErr)
+
+#prediction model
+rawfeats = pd.read_csv('data/dengue_features_test.csv')
+sj_validation = rawfeats[rawfeats.city=='sj'].copy()
+sj_validation = sj_validation[attributes]
+sj_validation = sj_validation.interpolate(method='linear', limit_direction='forward')
+y_validation_mlr_sj= mlr.predict(sj_validation)
+y_validation_mlr_sj = y_validation_mlr_sj.astype(int)
+
+
+#---iq---
+
+#Setting the value for X and Y
+attributes = ['reanalysis_dew_point_temp_k', 'reanalysis_specific_humidity_g_per_kg', 'station_avg_temp_c',  'station_min_temp_c']
+x = iq[attributes]
+y = iq['total_cases']
 
 #Splitting the dataset
 from sklearn.model_selection import train_test_split
@@ -31,13 +92,13 @@ print(list(zip(x, mlr.coef_)))
 
 #Prediction of test set
 y_pred_mlr= mlr.predict(x_test)
-#Predicted values
-#print("Prediction for test set: {}".format(y_pred_mlr))
+
+#Graphs
+examine(y_pred_mlr,y_test)
 
 #Actual value and the predicted value
 mlr_diff = pd.DataFrame({'Actual value': y_test, 'Predicted value': y_pred_mlr})
 print(mlr_diff.head())
-
 
 #Model Evaluation
 from sklearn import metrics
@@ -48,3 +109,16 @@ print('R squared: {:.2f}'.format(mlr.score(x,y)*100))
 print('Mean Absolute Error:', meanAbErr)
 print('Mean Square Error:', meanSqErr)
 print('Root Mean Square Error:', rootMeanSqErr)
+
+#Prediction model
+rawfeats = pd.read_csv('data/dengue_features_test.csv')
+iq_validation = rawfeats[rawfeats.city=='iq'].copy()
+iq_validation = iq_validation[attributes]
+iq_validation = iq_validation.interpolate(method='linear', limit_direction='forward')
+y_validation_mlr_iq= mlr.predict(iq_validation)
+y_validation_mlr_iq = y_validation_mlr_iq.astype(int)
+
+#Submission
+submission = pd.read_csv('data/submission_format.csv',index_col=[0, 1, 2])
+submission.total_cases = np.concatenate([y_validation_mlr_sj, y_validation_mlr_iq])
+submission.to_csv("submissions/mlr.csv")
